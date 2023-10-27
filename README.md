0f8f11e5d653683f7e095883ad6c7c0fc014226f
Harsh-Codes-382 <harshkumar3826@gmail.com>
2023-10-27 19:33:45 +0530

    This is description about Project

---
+++ README.md
@@ -0,0 +1,47 @@
+**Project Title: MERN Stack Note-Keeping App**
+
+**About:**
+
+#The MERN Stack Notes Saver App is a full-stack web application that provides users with a convenient platform to store and manage their notes. Built using the popular MERN (MongoDB, Express.js, React, Node.js) stack, this application offers a seamless user experience for note creation, management, and storage.
+
+**Key Features:**
+
+1. **User Authentication:** The app allows users to register, log in, and securely authenticate themselves. This ensures that each user has their own private space for creating and managing notes.
+
+2. **Note Creation and Management:** Users can create, edit, and delete notes. The application offers a user-friendly interface for adding and organizing notes, making it easy to keep important information at your fingertips.
+
+3. **Data Persistence:** Notes are stored securely in a MongoDB database, ensuring data integrity and reliability. Users can trust that their notes will remain safe and accessible whenever they need them.
+
+4. **User-Friendly Interface:** The project features a responsive and intuitive UI/UX design that is accessible across devices, making it convenient for users to access their notes on the go.
+
+5. **Logout Functionality:** Users have the option to log out, ensuring the privacy and security of their notes. 
+
+6. **Create and Edit Notes:** Users can create and edit notes effortlessly. The intuitive and user-friendly interface allows you to write, format, and organize your notes as per your preferences.
+
+7. **Search and Filter**: Easily find your notes using the search and filter functionality. Sort and categorize your notes to quickly locate the information you need.
+
+8. **Delete Notes**: If you no longer need a note, you can simply delete it. The application offers a straightforward way to remove notes you've created.
+
+9. **Responsive Design**: Our application is designed to be responsive and accessible on various devices, ensuring a seamless user experience on both desktop and mobile platforms.
+
+**Tech Stack:**
+
+- **Frontend:** Built with React, a popular JavaScript library for building user interfaces. It offers a dynamic and interactive user experience.
+
+- **Backend:** Developed using Node.js and Express.js, providing a robust and scalable server-side architecture. It handles user authentication, note management, and communication with the database.
+
+- **Database:** MongoDB is used to store and manage user data and notes efficiently. It's a NoSQL database, allowing flexibility in data structure and scalability.
+
+- **User Authentication:** Authentication is implemented using JWT (JSON Web Tokens) to ensure secure and stateless authentication.
+
+- **Styling:** Utilizes modern styling libraries and CSS for a polished and visually appealing interface.
+
+**Benefits:**
+
+The MERN Stack Note-Keeping App is an excellent tool for individuals who need a simple and effective way to organize their thoughts, ideas, to-do lists, and more. It provides a secure environment to store and access notes from anywhere. Whether you're a student, professional, or anyone in need of a digital notepad, this application can help you streamline your note-keeping process.
+
+**Get Started:**
+
+Experience the convenience of organized note-keeping with our MERN Stack Note-Keeping App. Sign up, log in, and start saving your notes today!
+
+---
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,78 +2,67 @@ import { useState } from 'react';
 import NoteContext from './noteContext';
 
 const NoteState = (props) =>{
-    // const s1 = {             // yha voh state ayegi jisse hum different diff. components mai bhejna chahate hai
-    //     "name":"Harsh",
-    //     "class":"5d"
-    // }
-    // const [state, setstate] = useState(s1);
-    // const update = () =>{   // this is func who update the state variable
-    //     setTimeout(()=>{
-    //         setstate({           // now we are setting the new state value
-    //             "name":"rohit",
-    //             "class":"2d"
-    //         })
-    //     },1000)
-    // }
+
     const host = 'http://localhost:5000';
     const notesinitial = [];// for temporary we hardcore the notes
      
       const [notes,setnotes]  = useState(notesinitial)
-      const getnotes = async () =>{ // this func will fetch all notes
-        // API Call
-        const   response = await fetch(`${host}/api/notes/fetchnotes`,{ // their url acc. to our backend in thunderclient
-            method:"GET",    // method like "POST", "GET", "PUT"
+      const getnotes = async () =>{ 
+
+        const   response = await fetch(`${host}/api/notes/fetchnotes`,{ 
+            method:"GET",    
             headers:{
-                'Content-Type':'application/json', // as we use in backend
-                'auth-token':localStorage.getItem('token') // we are taking the auth-token of user who logged in and his token got stored in localstorage so we are taking token  from there
+                'Content-Type':'application/json', 
+                'auth-token':localStorage.getItem('token')
             }
         });
         const json = await response.json();
-        console.log(json);  // now we can see the data of user of above particular token user
         setnotes(json); // now we are passing all notes which stored at this user token into the state variable who carries the array & it also passed to components to show on frontend
     }
     // Add a note
     const addnote = async (title, description, tag) =>{
-        // API Call
+
         const   response = await fetch(`${host}/api/notes/addnote`,{
-            method:"POST",    // method like "POST", "GET", "PUT"
+            method:"POST",  
             headers:{
-                'Content-Type':'application/json', // as we use in backend
-                'auth-token':localStorage.getItem('token') // we are taking the auth-token of user who logged in and his token got stored in localstorage so we are taking token  from there
+                'Content-Type':'application/json', 
+                'auth-token':localStorage.getItem('token') 
             },
             body: JSON.stringify({title, description, tag})  // we converting the json into the string
         });
         const note = await response.json(); // we are storing the response data into "note" variable and concat it into the state variable array.
         setnotes(notes.concat(note))    // concat upadtes an array & push returns an array
     }
+
     // Delete a note
     const deletenote = async (id) =>{ 
-        const response = await fetch(`${host}/api/notes/deletenote/${id}`,{ // we are passing the id of note which we want to delete
-            method:"DELETE",    // method like "POST", "GET", "PUT"
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`,{ 
+            method:"DELETE",    
             headers:{
-                'Content-Type':'application/json', // as we use in backend
-                'auth-token':localStorage.getItem('token') // we are taking the auth-token of user who logged in and his token got stored in localstorage so we are taking token  from there
+                'Content-Type':'application/json', 
+                'auth-token':localStorage.getItem('token') 
             }
         });
+
         const json = await response.json();
-        console.log(json);
+
         const newnotes = notes.filter((note)=>{ return note._id !== id}) // means agr iske andar ki id parameter same nhi huyi baaki array ki id se
         // then condition true hai & voh vale element array mai rhenge or jinki id match krgyi tbh condition false & voh array mai nhi rhenge means delete
         setnotes(newnotes);
     }
     // Edit a note
     const editnote = async (id, title, description, tag) =>{
-        // API call 
+        
         const   response = await fetch(`${host}/api/notes/updatenote/${id}`,{
-            method:"PUT",    // method like "POST", "GET", "PUT"
+            method:"PUT",    
             headers:{
-                'Content-Type':'application/json', // as we use in backend
-                'auth-token':localStorage.getItem('token')  // we are taking the auth-token of user who logged in and his token got stored in localstorage so we are taking token  from there 
+                'Content-Type':'application/json', 
+                'auth-token':localStorage.getItem('token')  
             },
-            body: JSON.stringify({title, description, tag})  // we converting the json into the string
+            body: JSON.stringify({title, description, tag})  
         });
         const json = await response.json();
-        console.log(json);
+
         // newnotes will hold the deep copy of state variable notes array
         let newnotes = JSON.parse(JSON.stringify(notes));
         // Client side updation
